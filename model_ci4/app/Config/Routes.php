e745164f62241f25113861cab588bd93582967cb
imamady <imamadiwijaya@unipdu.ac.id>
2024-04-05 04:51:59 -0700

    create model detail in ci

---
+++ model_ci4/app/Config/Routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/pages', 'Pages::index');
+$routes->get('/pages/about', 'Pages::about');
+$routes->get('/pages/contact', 'Pages::contact');
+$routes->get('/buku', 'Buku::index');
+$routes->get('/buku/(:segment)', 'Buku::detail/$1');
